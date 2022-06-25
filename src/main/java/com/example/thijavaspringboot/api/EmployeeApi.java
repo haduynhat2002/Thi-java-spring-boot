a7d6427bf89e4a05ad4423d60205d68a087e7e26
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-06-25 11:02:28 +0700

    update cross

--- src/main/java/com/example/thijavaspringboot/api/EmployeeApi.java
+++ src/main/java/com/example/thijavaspringboot/api/EmployeeApi.java
@@ -5,11 +5,13 @@ import com.example.thijavaspringboot.servicve.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
+@CrossOrigin("*")
 @Controller
 @RequestMapping("api/v1/employees")
 public class EmployeeApi {
